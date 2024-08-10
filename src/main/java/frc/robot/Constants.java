c47fbcee00df098c6af2f62e28d2669137188899
Millie-Patel <138508703+Manyata-Patel@users.noreply.github.com>
2024-08-09 18:39:20 -0700

    MP - Shooter code for testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,8 +13,8 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
     
     public static final int kDriverControllerPort = 0;
-  }
+    public static final int kOperatorControllerPort = 1;
+ 
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,49 +1,282 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
+// import com.pathplanner.lib.auto.AutoBuilder;
+// import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+// import frc.robot.auto.Autonomous;
+// import frc.robot.auto.DriveDistance;
+// import frc.robot.commands.DriveCommands;
+// import frc.robot.subsystems.arm.Arm;
+// import frc.robot.subsystems.drive.Drive;
+// import frc.robot.subsystems.drive.GyroIO;
+// import frc.robot.subsystems.drive.GyroIOPigeon2;
+// import frc.robot.subsystems.drive.ModuleIO;
+// import frc.robot.subsystems.drive.ModuleIOSim;
+// import frc.robot.subsystems.drive.ModuleIOSparkMax;
+// import frc.robot.subsystems.intake.Intake;
+// import frc.robot.subsystems.leds.LEDs;
+import frc.robot.subsystems.shooter.Shooter;
+// import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+  // Subsystems
+ // private final Drive drive;
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // Controller
+  private static final CommandXboxController driver =
+      new CommandXboxController(Constants.kDriverControllerPort);
+  private static final CommandXboxController operator =
+      new CommandXboxController(Constants.kOperatorControllerPort);
+
+  // private static final LEDs leds = new LEDs();
+  // public static final Arm arm = new Arm(operator.getHID(), leds);
+
+  // private static Intake intake;
+  private static Shooter shooter;
+  // private static Autonomous autonomous;
+
+  // Dashboard inputs
+  // private final LoggedDashboardChooser<Command> autoChooser;
+
+  public Command ampButtonBinding() {
+    return new SequentialCommandGroup(
+        new ParallelCommandGroup(new InstantCommand(() -> shooter.setAmpSpeed())),
+        new InstantCommand(() -> System.out.println("Arm to amp pos...")),
+        new WaitCommand(1.0),
+        new InstantCommand(() -> System.out.println("Intaking...")));
+  }
+
+  public Command shootButtonBinding() {
+    return new SequentialCommandGroup(
+        new ParallelCommandGroup(new InstantCommand(() -> shooter.setSpeakerSpeed())),
+        new InstantCommand(() -> System.out.println("Arm to shoot pos...")),
+        new WaitCommand(1.0),
+        new InstantCommand(() -> System.out.println("Intaking...")));
+  }
+  ;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
+
+    //intake = new Intake();
+    shooter = new Shooter();
+
+ //   leds.ladyChaser();
+
+    // Manual:
+    // arm.setDefaultCommand(new ArmCommand(() -> operator.getRightY(), arm));
+
+    // switch (Constants.currentMode) {
+    //   case REAL:
+    //     // Real robot, instantiate hardware IO implementations
+    //     drive =
+    //         new Drive(
+    //             new GyroIOPigeon2(),
+    //             new ModuleIOSparkMax(0),
+    //             new ModuleIOSparkMax(1),
+    //             new ModuleIOSparkMax(2),
+    //             new ModuleIOSparkMax(3));
+    //     break;
+
+    //  case SIM:
+        // Sim robot, instantiate physics sim IO implementations
+      //   drive =
+      //       new Drive(
+      //           new GyroIO() {},
+      //           new ModuleIOSim(),
+      //           new ModuleIOSim(),
+      //           new ModuleIOSim(),
+      //           new ModuleIOSim());
+      //   break;
+
+      // default:
+        // Replayed robot, disable IO implementations
+    //     drive =
+    //         new Drive(
+    //             new GyroIO() {},
+    //             new ModuleIO() {},
+    //             new ModuleIO() {},
+    //             new ModuleIO() {},
+    //             new ModuleIO() {});
+    //     break;
+    // }
+
+  //  autonomous = new Autonomous(intake, shooter, drive);
+
+    // Set up auto routines
+   // NamedCommands.registerCommand("Run Shoot", autonomous.SHOOT());
+
+   // autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
+    // Set up autonomous pathplanner routines
+  //  autoChooser.addOption("Auto: SHOOT", autonomous.SHOOT());
+    // autoChooser.addOption("Auto: PL-MB", autonomous.PL_MB());
+    // autoChooser.addOption("Auto: PL-MB-1P", autonomous.PL_MB_1P(0));
+    // autoChooser.addOption("Auto: PL-MB-1P-L", autonomous.PL_MB_1P_L());
+    // autoChooser.addOption("Auto: PL-MB-2P", autonomous.PL_MB_2P());
+    // autoChooser.addOption("Auto: Auto PL-MB-1L", AutoBuilder.buildAuto("PL-M-1L"));
+    // autoChooser.addOption("Auto: Test", autonomous.test());
+    // autoChooser.addOption("Mobility", autonomous.MOBILITY());
+    // autoChooser.addOption("Mobility-Shoot", autonomous.SHOOT_MOBILITY());
+    // autoChooser.addOption("2P-Mobility", autonomous.SHOOT_MOBILITY_LOAD());
+
+    // Configure the button bindings
+    configureButtonBindings();
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-   
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
+  // -------------------------------------------------------------------[Button
+  // Bindings]-----------------------------------------------------------------------
+
+  private void configureButtonBindings() {
+
+    // right bumper intakes note and retracts to move note away from shooter wheels
+    operator
+        .leftBumper()
+        .whileTrue(
+            new InstantCommand(
+                () -> {
+                  System.out.println("Intaking...");
+                }))
+        .onFalse(new InstantCommand(
+          () -> System.out.println("Intake retracting...")));
+
+    // 'a' button outtakes note
+    operator
+        .rightBumper()
+        .whileTrue(new InstantCommand(() -> System.out.println("Outtaking...")))
+        .onFalse(new InstantCommand(() -> System.out.println("Intake zeroed...")));
+
+    // driver.y().onTrue(autonomous.SHOOT_MOBILITY());
+
+    /*// Left bumper shoots Speaker, now shootSpeaker also includes moving intake also.
+    operator
+        .leftTrigger()
+        .whileTrue(shooter.shootSpeaker())
+        .onFalse(
+            new InstantCommand(
+                () -> {
+                  shooter.zero();
+                  intake.zero();
+                }));
+
+    // Left joystick CLICK shoots amp, also moves intake using one button
+    operator
+        .rightTrigger()
+        .whileTrue(shooter.shootAmp())
+        .onFalse(
+            new InstantCommand(
+                () -> {
+                  shooter.zero();
+                  intake.zero();
+                }));*/
+
+    operator
+        .povUp()
+        .whileTrue(new InstantCommand(() -> shooter.setSpeedReverse()))
+        .onFalse(
+            new InstantCommand(
+                () -> {
+                  shooter.zero();
+                  System.out.println("Intake zeroed...");
+                }));
+
+    operator
+        .a()
+        .whileTrue(
+            new InstantCommand(
+                () -> {
+                  System.out.println("Arm to climb down pos...");
+                }));
+
+    operator
+        .b()
+        .whileTrue(ampButtonBinding())
+        .onFalse(
+            new ParallelCommandGroup(
+                new InstantCommand(() -> System.out.println("Arm to shoot pos...")),
+                new InstantCommand(() -> System.out.println("Intake zeroed...")),
+                new InstantCommand(() -> shooter.zero())));
+
+    operator
+        .y()
+        .whileTrue(
+            new InstantCommand(
+                () -> {
+                  System.out.println("Arm to climb up pos...");
+                }));
+
+    operator
+        .x()
+        .whileTrue(shootButtonBinding())
+        .onFalse(
+            new ParallelCommandGroup(
+                new InstantCommand(() -> System.out.println("Arm to shoot pos...")),
+                new InstantCommand(() -> System.out.println("Intake zeroed...")),
+                new InstantCommand(() -> shooter.zero())));
+
+    // drive.setDefaultCommand(
+    //     DriveCommands.joystickDrive(
+    //         drive, () -> -driver.getLeftY(), () -> -driver.getLeftX(), () -> driver.getRightX()));
+
+    // driver.leftBumper().onTrue(autonomous.SHOOT_MOBILITY_LOAD());
+
+    // driver.rightBumper().onTrue(leds.toggleBlue());
+
+    // driver.rightTrigger().onTrue(new InstantCommand(() -> arm.climbOff()));
+
+    // driver.a().onTrue(new InstantCommand(() -> Drive.resetGyro()));
+
+    /*
+    controller.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+
+    controller.b()
+        .onTrue(
+            Commands.runOnce(
+                    () ->
+                        drive.setPose(
+                            new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+                    drive)
+                .ignoringDisable(true));
+
+    controller.a()
+        .whileTrue(
+            Commands.startEnd(
+                () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel));
+    */
+
   }
 
   /**
@@ -51,8 +284,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return null;
-  }
+  // public Command getAutonomousCommand() {
+  //  // return autoChooser.get();
+  // }
 }
